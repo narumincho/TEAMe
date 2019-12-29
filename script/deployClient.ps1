74bf12d3825d88888ae7ee4ad12d4addd2c5e833
narumincho <narumincho.starfy@gmail.com>
2019-12-29 19:12:47 +0900

    delete deploy client script

--- script/deployClient.ps1
+++
@@ -1,7 +0,0 @@
-Write-Output "Compile Client Code And Upload Firebase Server";
-
-./node_modules/.bin/parcel.ps1 build ./source/call/call.ts --out-dir distribution --out-file main.js;
-
-Copy-Item ./source/assets/ ./distribution/ -Recurse -Force
-
-firebase.ps1 deploy --only hosting --project teame-c1a32;
